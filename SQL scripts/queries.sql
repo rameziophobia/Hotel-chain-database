c7b0e288fb83f142420e8ec7754f056ec594e27b
Ramez <rameznoshy@gmail.com>
2020-04-24 05:04:39 +0200

    add, fix queries and updates

--- SQL scripts/queries.sql
+++ SQL scripts/queries.sql
@@ -1,6 +1,42 @@
+-- List the guests participating in activities in the next week
+SELECT g.Name as guest, a.Name as activity, a.Price, a.Date
+FROM Activities a
+JOIN Participates_in part
+       ON a.Activity_ID = part.Activity_ID
+JOIN Guest g
+       ON g.SSN = part.guest_SSN
+WHERE
+       a.Date BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 1 WEEK);
+
+-- List all the reservations in the next 3 months
+SELECT *
+FROM Reservation
+WHERE Reservation.Start_date BETWEEN NOW() AND DATE_ADD(NOW(), INTERVAL 3 MONTH);
+
+-- List all facilities for each hotel .. el 2 ma3 ba3d fe 
+-- table wa7ed manfe3sh 3ashan fihom info mo5talifa
+
+-- List all gyms with their equipment for all hotels
+SELECT h.Name AS 'hotel name', g.Name AS 'gym name', 
+	   g.Working_hours, eq.Equipment_name
+FROM Hotel h
+LEFT JOIN Gym g
+	ON h.ID = g.Hotel_ID
+LEFT JOIN Equipment_name eq
+	ON eq.F_ID = g.Facility_ID;
+       
+-- List all restaurants for each hotel 
+SELECT  h.Name AS 'hotel name', r.Name AS 'restaurant name', 
+		r.Food_type, r.Price_range
+FROM Hotel h
+LEFT JOIN Restaurant r
+       ON h.ID = r.Hotel_ID;
+
+-- List all the available rooms with the discounted price for agencies
+
 -- note i added semicolon ba3d kol query 8er kda byedi error el dr alet 3aliha bardo 
 -- List all the available rooms of type double
--- suggestion: name bta3 min ?, nzawid 7war el dates
+-- suggestion: name bta3 min ?, nzawid 7war el dates??
 SELECT price, view, Name
 FROM   Room, Hotel
 WHERE  Available=1 AND Room Type="double" AND Hotel_ID=ID;
@@ -18,30 +54,3 @@ FROM   Reservee, Reservation, Room
 WHERE  ID = Reservee_ID AND 
        Room.Reservation_ID=Reservation.Reservation_ID
        AND Hotel_ID=1;
-
--- List all Facilities for each hotel 
-SELECT Hotel.Name AS 'hotel name', g.Name AS 'gym name', g.Working_hours,
-       r.Name AS 'restaurant name', r.Food_type, r.Price_range
-FROM Hotel h
-LEFT JOIN Gym g
-       ON h.ID = g.Hotel_ID
-LEFT JOIN Restaurant r
-       ON h.ID = r.Hotel_ID;
-
--- List all the reservations in the next 3 months
-SELECT *
-FROM Reservation
-WHERE Reservation.Start_date BETWEEN NOW() AND DATEADD(MONTH, 3, NOW()) 
-
--- List all the available rooms with the discounted price for agencies
-
--- List the guests participating in activities in the next week
-SELECT g.Name as guest, a.Name as activity, a.Price
-FROM Activities a
-LEFT JOIN Participates_in part
-       ON a.ID = part.Activity_ID
-LEFT JOIN Guest g
-       ON g.SSN = part.Activity_ID
-WHERE
-       a.Start_date BETWEEN NOW() AND DATEADD(WEEK, 1, NOW());
-
--- SQL scripts/update.sql
+++ SQL scripts/update.sql
@@ -4,8 +4,13 @@ JOIN Department d
 	ON d.Department_ID = e.Department_ID
 JOIN Hotel h
 	ON d.Hotel_ID = h.ID
-SET Employee.Salary = Employee.Salary * 1.15
+SET e.Salary = e.Salary * 1.15
 WHERE h.Rating = 5;
+
+-- update rooms with id 6, 7 with the reservation id of 4
+UPDATE Room
+SET Room.Reservation_ID = 4
+WHERE Room.Room_ID IN (6, 7);
 
 -- update room prices reserved by agencies using their discount%
 UPDATE Room
@@ -15,19 +20,20 @@ JOIN Booking_Agency
     ON Reservation.Reservee_ID = Booking_Agency.Reservee_ID
 SET Room.Price = Room.Price * (1 - Booking_Agency.Discount);
 
--- update activity time of activity 1 to now
-UPDATE Activity
-SET Activity.Date = NOW()
-WHERE Activity.Activity_ID = 1;
+-- update activity time of activity 1 to tommorow
+UPDATE Activities a
+SET a.Date = DATE_ADD(NOW(), INTERVAL 1 DAY)
+WHERE a.Activity_ID = 1;
 
--- update rooms with id 1, 2 with the reservation id of 1
-UPDATE Room
-SET Room.Reservation_ID = 1
-WHERE Room.Room_ID IN (1, 2);
-
+-- update manager ssn of all emplyees with manager id x to manager id y
+SET @old_mngr = 1254;
+SET @new_mngr = 5641;
+UPDATE Employee e
+SET e.Manager_SSN =  @new_mngr
+WHERE e.Manager_SSN = @old_mngr;
 
 -- increment the rating of hotels which have 2 or more facilities by 1
--- TODO that wont work
+-- TODO that implementation doesnt work .. mksl azbot-ha
 UPDATE Hotel
 SET Hotel.Rating = Hotel.Rating + 1
 WHERE Hotel.Rating < 5 AND
@@ -39,6 +45,4 @@ WHERE Hotel.Rating < 5 AND
 	JOIN Restaurant r
 		   ON h.ID = r.Hotel_ID
 	HAVING COUNT(*) > 1
-	)
-
-
+	);
