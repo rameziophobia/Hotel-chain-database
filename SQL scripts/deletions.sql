73a232735ed537e9bea6d355be73034b2e1095cb
hlaahmed <40806663+hlaahmed@users.noreply.github.com>
2020-04-29 22:42:46 +0200

    Update deletions.sql

--- SQL scripts/deletions.sql
+++ SQL scripts/deletions.sql
@@ -2,13 +2,13 @@ DELETE FROM Guest
 WHERE  SSN = "653298653";
 
 DELETE FROM Reservation
-WHERE  Reservee_ID = 9;
-
-DELETE FROM Hotel
-WHERE  Rating < 3;
+WHERE  Reservee_ID = 3;
 
 DELETE FROM Activities
 WHERE  Date < CURDATE();
 
-DELETE FROM ROOM
-WHERE  Available = 0;
+DELETE FROM Employee
+WHERE  Salary = 5000;
+
+DELETE FROM Equipment
+WHERE  Equipment = 'Chest press machine';
